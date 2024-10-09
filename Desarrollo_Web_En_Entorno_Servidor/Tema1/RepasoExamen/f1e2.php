585fc88f7c40eb8f417eddb293b9a3272a5dd9ad
Alejandro~ <aricsol590@g.educaand.es~>
2024-10-09 16:13:28 +0200

    RepasoExamen

--- Desarrollo_Web_En_Entorno_Servidor/Tema1/RepasoExamen/f1e2.php
+++ Desarrollo_Web_En_Entorno_Servidor/Tema1/RepasoExamen/f1e2.php
@@ -1,28 +1,23 @@
 <?php
-    $meses = array("Enero ","Febrero ","Marzo ","Abril ","Mayo ","Junio ","Julio ","Agosto ","Septiembre ","Octubre ","Noviembre ","Diciembre ");
-/*
-    $meses[0][0] = "Primavera";
-    $meses[1][0] = "Verano";
-    $meses[2][0] = "Otoño";
-    $meses[3][0] = "Invierno";
-    $meses[3][1] = "Enero";
-    $meses[3][2] = "Febrero";
-    $meses[3][3] = "Marzo";
-    $meses[0][1] = "Abril";
-    $meses[0][2] = "Mayo";
-    $meses[0][3] = "Junio";
-    $meses[1][1] = "Julio";
-    $meses[1][2] = "Agosto";
-    $meses[1][3] = "Septiembre";
-    $meses[2][1] = "Octubre";
-    $meses[2][2] = "Noviembre";
-    $meses[2][3] = "Diciembre";
-print_r($meses);
-*/
-//Los meses son las claves y las estaciones los valores
-$primavera = $meses[3].$meses[4].$meses[5];
-$verano = $meses[6].$meses[7].$meses[8];
-$otoño = $meses[9].$meses[10].$meses[11];
-$invierno = $meses[0].$meses[1].$meses[2];
-echo $primavera;
+    $array=array(
+        "Abril" => "Primavera",
+        "Mayo"=> "Primavera",
+        "Junio"=> "Primavera",
+        "Julio" => "Verano",
+        "Agosto"=> "Verano",
+        "Septiembre"=> "Verano",
+        "Octubre"=> "Otoño",
+        "Noviembre"=> "Otoño",
+        "Diciembre"=> "Otoño",
+        "Enero"=> "Invierno",
+        "Febrero"=> "Invierno",
+        "Marzo"=> "Invierno",
+    );
+    print_r($array);
+    foreach ($array as $key => $value) {
+        if($value == "Primavera"){
+            print($key."\n");
+        }
+        print($value);
+    }
 ?>
--- Desarrollo_Web_En_Entorno_Servidor/Tema1/RepasoExamen/f1e4.php
+++ Desarrollo_Web_En_Entorno_Servidor/Tema1/RepasoExamen/f1e4.php
@@ -1,9 +1,68 @@
 <?php
-    $ingredientes = "Pan,Carne,Queso,Tomate";
-    $array=explode(",",$ingredientes);
-    foreach ($array as $key) {
-        echo $key." ";
+   class Fruta{
+    private $nombre;
+    private $sabor;
+    private $color;
+
+    function __construct($nombre,$sabor,$color){
+        $this->nombre = $nombre;
+        $this->sabor = $sabor;
+        $this->color = $color;
     }
-    echo "\n";
-    print_r($array);
+    function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+    function setSabor($sabor){
+        $this->sabor = $sabor;
+    }
+    function setColor($color){
+        $this->color = $color;
+    }
+    function getNombre(){
+        return $this->nombre;
+    }
+    function getSabor(){
+        return $this->sabor;
+    }
+    function getColor(){
+        return $this->color;
+    }
+    function __toString(){
+        return "El nombre de la fruta es: $this->nombre\nEl sabor de la fruta es: $this->sabor\nEl color de la fruta es: $this->color\n";
+    }
+   }
+   function paraZumo(Fruta $fr){
+    $random = rand(0,1);
+    if ($random == 0) {
+        echo "El/la ". $fr->getNombre(). " es apto/a para hacer zumo\n";
+    }else{
+        echo "El/la ". $fr->getNombre(). " no es apto/a para hacer zumo\n";
+    }
+   }
+   $fruta1 = new Fruta("Fresa","Roja","Dulce");
+   $fruta2 = new Fruta("Limón","Amarillo","Ácido");
+   echo $fruta1;
+   echo $fruta2->getNombre()."\n";
+   echo $fruta2->getSabor();
+   $fruta1->setSabor("Mierda");
+   echo $fruta1;
+   echo paraZumo($fruta1);
+   class Mediterranea extends Fruta{
+    public $textura;
+    function __construct($nombre,$sabor,$color,$textura){
+        $this->textura = $textura;
+        parent::__construct($nombre,$sabor,$color);
+    }
+    function __toString(){
+        return parent::__toString()."La textura de la fruta es: $this->textura\n";
+    }
+   }
+   $fr = new Mediterranea("Sandía","Dulce","Verde","Suave");
+   echo $fr;
+   $fr->setNombre("melón");
+   echo $fr;
+   $fr->textura = "Cabron";
+   echo $fr;
+   $fr->setColor("Rosa Coño");
+   echo $fr;
 ?>
